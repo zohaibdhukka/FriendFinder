e68ce2b25c5ac22edf50b367c24b80e13b909990
zohaibdhukka <zohaibdhukka@gmail.com>
2019-03-07 19:38:30 -0600

    third Commit $.post getting errorthird commit but getting $.post not a function error

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,6 +2,7 @@ var path = require("path");
 var friends = require("../data/friends.js");
 
 module.exports = function(app) {
+    
     app.get("/api/friends", function(req, res){
         res.json(friends);
     })
