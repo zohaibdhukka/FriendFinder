2001f6edc6d51b50c5b722b6455f09265f4e1642
zohaibdhukka <zohaibdhukka@gmail.com>
2019-03-07 19:27:11 -0600

    third Commit $.post getting error

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,8 +1,36 @@
-var path = require('path');
+var path = require("path");
 var friends = require("../data/friends.js");
 
 module.exports = function(app) {
     app.get("/api/friends", function(req, res){
         res.json(friends);
     })
+
+    app.post("/api/friends", function(req, res) {
+
+        var newFriend = {
+            name: req.body.name,
+            photo: req.body.photo,
+            scores: JSON.parse(req.body.scores)
+        }
+
+        var newArray = [];
+
+        friends.forEach(function(item, index) {
+            var difference = 0;
+            for (var i = 0; i < item.scores.length; i++) {
+                difference += Math.abs(item.scores[i] - newFriend.scores[i]);
+            }
+            newArray.push({ "difference": difference, "index": index });
+        });
+
+        newArray.sort(function(a, b) {
+            return a.difference - b.difference;
+        });
+
+        friends.push(newFriend);
+        res.json(friends[newArray[0].index]);
+
+    });
+
 }
